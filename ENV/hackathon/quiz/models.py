25e3e79cda165ed48332710bf7b9cbe6134ea64a
System Administrator <root@MacBook-Pro.local>
2016-04-16 14:08:56 +0100

    Integrated front end CSS/html/images

--- ENV/hackathon/quiz/models.py
+++ ENV/hackathon/quiz/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 from django.forms import ModelForm
 
-# Create your models here.
 
+# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -13,22 +13,18 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     correct_choice = models.BooleanField(default=0)
+
     def __str__(self):
         return self.choice_text
 
-class Tip(models.Model):
 
+class Tip(models.Model):
     tip = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
 
     def __str__(self):
         return self.tip
-
-class TipForm(ModelForm):
-    class Meta:
-        model = Tip
-        fields = ['tip', 'location']
