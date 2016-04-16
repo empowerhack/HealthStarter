d28c41c28dfd517ae42ab51f95cd117f5ee2e186
System Administrator <root@MacBook-Pro.local>
2016-04-16 14:08:56 +0100

    Integrated front end

--- ENV/hackathon/quiz/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Tip
-
-class Tip(forms.Form):
-    name = forms.CharField(max_length=100)
-    authors = forms.ModelMultipleChoiceField(queryset=Tip.objects.all())
--- ENV/hackathon/quiz/views.py
+++ ENV/hackathon/quiz/views.py
@@ -3,18 +3,22 @@ from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'quiz/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'quiz/detail.html', {'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=(question_id + 1))
     return render(request, 'quiz/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -28,13 +32,12 @@ def vote(request, question_id):
         })
     else:
         if selected_choice.correct_choice:
-                return render(request, 'quiz/correct.html', {'question': question})
+            return render(request, 'quiz/correct.html', {'question': question})
         else:
-                return render(request, 'quiz/incorrect.html', {'question': question})
+            return render(request, 'quiz/incorrect.html', {'question': question})
 
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('quiz:results', args=(question.id,)))
-
