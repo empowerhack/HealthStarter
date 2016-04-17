4744d3368ac7bc0e264a35a64e5af0456a8cbdf0
ashleypeacock <avpeacock@gmail.com>
2016-04-17 15:22:06 +0100

    Final integration : - quiz UI updated to use same template as the index page - sends user to result page - number of correct answers printed - quiz button goes to quiz

--- ENV/hackathon/quiz/urls.py
+++ ENV/hackathon/quiz/urls.py
@@ -7,6 +7,6 @@ app_name = 'quiz'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url('^results', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- ENV/hackathon/quiz/views.py
+++ ENV/hackathon/quiz/views.py
@@ -15,9 +15,18 @@ def detail(request, question_id):
     return render(request, 'quiz/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=(question_id + 1))
-    return render(request, 'quiz/results.html', {'question': question})
+def results(request):
+    correct_choices = 0
+    choices = request.POST.values()[1:]
+    total_choices = len(choices)
+
+    for choice_id in choices:
+        id = int(choice_id)
+        choice = get_object_or_404(Choice, pk=id)
+        if choice.correct_choice:
+            correct_choices = correct_choices + 1
+
+    return render(request, 'quiz/results.html', {"correct_answers" : correct_choices, "total_choices" : total_choices})
 
 
 def vote(request, question_id):
